8249adefb8065d224b963574dbe0b64deb8279e1
Goketsu <benjamin.viraize@gmail.com>
2016-12-01 19:09:46 +0100

    mise en place du git

---
+++ Watch/Main.cpp
@@ -0,0 +1,127 @@
+#include <Windows.h>
+#include <GL\freeglut.h>
+
+//#include <gl\GL.h>
+//#include <GL\GL.h>
+
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+
+#include "ModuleCouleurs.h"
+#include "ModuleManipulateur.h"
+#include "ModuleMenus.h"
+#include "ModuleReshape.h"
+
+// gestion des lumieres et melanges
+void myInit(void)
+{
+	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+	glEnable(GL_LIGHTING);
+	// on defini les parametres de la source 0
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, couleurNoir());
+	glLightfv(GL_LIGHT0, GL_SPECULAR, couleurNoir());
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, couleurBlanc());
+	glEnable(GL_LIGHT0);
+
+	// definition de façon de traiter les faces
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	// gestion des parametres de transparence
+	glEnable(GL_ALPHA_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_DST_ALPHA);
+
+	// façon de gerer le modele d'eclairage
+	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+
+}
+
+// dessin du premier cube
+void display1(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	manipulateurSouris();
+	manipulateurClavier();
+	glPushMatrix();
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, couleurRouge(0.5f));
+	glScalef(17.0, 5.0, 10.0);
+	glutSolidCube(1.0);
+	glPopMatrix();
+	glPopMatrix();
+}
+
+// dessin du deuxieme cube
+void display2(void)
+{
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	manipulateurSouris();
+	manipulateurClavier();
+	glPushMatrix();
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, couleurBlanc(0.5f));
+	glScalef(9.0, 9.0, 18.0);
+	glutSolidCube(1.0);
+	glPopMatrix();
+	glPopMatrix();
+}
+
+// dessin du deuxieme cube
+void display3(void)
+{
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	manipulateurSouris();
+	manipulateurClavier();
+	glPushMatrix();
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, couleurBleu(0.5f));
+	glScalef(9.0, 9.0, 9.0);
+	glutSolidSphere(1.25, 50, 50);
+	glPopMatrix();
+	glPopMatrix();
+}
+
+void display(void)
+{
+	glEnable(GL_DEPTH_TEST);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	display1();
+	display2();
+	display3();
+	glPopMatrix();
+	glFlush();
+	glutSwapBuffers();
+
+}
+
+int main(int argc, char** argv)
+{
+	/* Gestion de creation de fenetre*/
+	glutInit(&argc, argv);
+	// on travaille en rgba avec un double buffer et en profondeur
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowPosition(300, 300);
+	glutInitWindowSize(250, 250);
+	glutCreateWindow("Test de transparence");
+	myInit();
+	creationMenuBasique();
+	setParametresOrthoBasique(-11.0, 11.0, -11.0, 11.0, -500.0, 500.0);
+	setManipulateurDistance(1.0f);
+	glutReshapeFunc(reshapeOrthoBasique);
+	glutKeyboardFunc(keyBasique);
+	glutSpecialFunc(specialBasique);
+	glutMotionFunc(motionBasique);
+	glutMouseFunc(sourisBasique);
+	glutDisplayFunc(display);
+
+	// entrée de la boucle principale
+	glutMainLoop();
+	return(0);
+}
